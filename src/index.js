dc1c2cd77a18956e91205bc9d92378ada7c31544
yunfengsay <fdoctor00@gmail.com>
2021-12-27 20:02:33 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -26,4 +26,5 @@ const mouse = robot.getMousePos();
 
 // Get pixel color in hex format.
 const hex = robot.getPixelColor(mouse.x, mouse.y);
+
 console.log("#" + hex + " at x:" + mouse.x + " y:" + mouse.y);
